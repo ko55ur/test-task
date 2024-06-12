bf53d6b7fa733f9494cf9d7c87568de28abd7526
ko55ur <96375951+ko55ur@users.noreply.github.com>
2024-06-13 01:30:13 +0300

    fix

--- site-script/webcheck.sh
+++ site-script/webcheck.sh
@@ -50,11 +50,11 @@ function stdError {
 # Проверяем соединение с интернетом переда запросом к сайту
 if ping -q -w 1 -c 1 8.8.8.8 > /dev/null 2>&1; then
     stdOutput "Соединение с интернетом OK"
-    HTTPCODE=$(curl --max-time 5 --silent --write-out %{response_code} --output "$STDOUTFILE" "$WEBSITE")
+    HTTPCODE=$(curl --max-time 5 --silent --write-out "%{response_code}" --output "$STDOUTFILE" "$WEBSITE")
 # Если получили код 200 или 302, то уже ищем нужный нам контент
     if [[ "$HTTPCODE" -eq 200 ]] || [[ "$HTTPCODE" -eq 302 ]]; then
         stdOutput "HTTP STATUS CODE $HTTPCODE -> OK"
-	    wget -q "$WEBSITE" -O - | grep -qi "$FINDCONTENT" && echo "Страница содержит искомый текст -> "$FINDCONTENT" " || echo "Страница не содержит искомый текст"
+	    wget -q "$WEBSITE" -O - | grep -qi "$FINDCONTENT" && echo "Страница содержит искомый текст -> \"$FINDCONTENT\"" || echo "Страница не содержит искомый текст"
     else
         stdError "Запрос завершился со статусом ошибки CODE $HTTPCODE?"
     fi
